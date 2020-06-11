a293ebedb713c0a21b318c9d8aeaf67f6fa428ec
Marko Sustarsic <marko.sustarsic@argos.co.uk>
2020-06-11 16:02:34 +0100

    Solution

--- src/main/java/myapps/LineSplit.java
+++ src/main/java/myapps/LineSplit.java
@@ -1,5 +1,6 @@
 package myapps;
 
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.common.serialization.Serdes;
@@ -7,19 +8,22 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
 
-public class Pipe {
+public class LineSplit {
 
   public static void main(String[] args) throws Exception {
     Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linesplit");
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
     final StreamsBuilder builder = new StreamsBuilder();
 
-    builder.stream("streams-pipe-input").to("streams-pipe-output");
+    KStream<String, String> source = builder.stream("streams-linesplit-input");
+    source.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+        .to("streams-linesplit-output");
 
     final Topology topology = builder.build();
     final KafkaStreams streams = new KafkaStreams(topology, props);
--- src/main/java/myapps/WordCount.java
+++ src/main/java/myapps/WordCount.java
@@ -1,25 +1,36 @@
 package myapps;
 
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 
-public class Pipe {
+public class WordCount {
 
   public static void main(String[] args) throws Exception {
     Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
     final StreamsBuilder builder = new StreamsBuilder();
 
-    builder.stream("streams-pipe-input").to("streams-pipe-output");
+    KStream<String, String> source = builder.stream("streams-wordcount-input");
+    source.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+        .groupBy((key, value) -> value)
+        .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
+        .toStream()
+        .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
 
     final Topology topology = builder.build();
     final KafkaStreams streams = new KafkaStreams(topology, props);
